571d57d26357ba41165b67f809f2c2e78d207610
unknown <yeraldycoronel0@gmail.com>
2026-02-09 11:35:47 -0500

    foro

---
+++ Proyecto.js
@@ -0,0 +1,19 @@
+function irA(seccion){
+    alert("Ir a: " + seccion);
+}
+
+function crearCuenta(){
+    alert("Crear nuevo foro");
+}
+
+function verCategoria(cat){
+    alert("Categoría: " + cat);
+}
+
+function anterior(){
+    alert("Página anterior");
+}
+
+function siguiente(){
+    alert("Página siguiente");
+}
